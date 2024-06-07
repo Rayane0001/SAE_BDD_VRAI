fa908c1cf27dc829dc65843399ad72272b6fa45a
Rousseau Rayane <rayane_rousseau@ens.univ-artois.fr>
2024-06-07 10:34:57 +0200

    sqdsfdfsdqfqsdqsdf

--- requetesSAE.sql
+++ requetesSAE.sql
@@ -24,3 +24,29 @@ GROUP BY
 	f.filiere_formation
 ORDER BY
 	total_candidatures_tech DESC;
+
+
+
+
+
+
+	SELECT
+        f.filiere_formation,
+        SUM(i.eff_tot_candi_femmes) AS total_candidatures_femmes
+    FROM
+        Infos_Formation_Etablissement i
+    JOIN
+        Formation f ON i.id_formation = f.id_formation
+    JOIN
+        Etablissement e ON i.code_uai_etab = e.code_uai_etab
+    JOIN
+        Departement d ON e.code_dept = d.code_dept
+    JOIN
+        Region r ON d.id_region = r.id_region
+    WHERE
+        r.nom_region = 'Hauts-de-France'
+    GROUP BY
+        f.filiere_formation
+    ORDER BY
+        total_candidatures_femmes DESC
+    LIMIT 10; -- Pour les filières les plus demandées par les femmes
