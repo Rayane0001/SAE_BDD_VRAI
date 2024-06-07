49b01df695319ba3bc45064947f9d34a7ed5ce2c
Rousseau Rayane <rayane_rousseau@ens.univ-artois.fr>
2024-06-07 10:41:06 +0200

    Rangement

--- requetesSAE.sql
+++ requetesSAE.sql
@@ -1,31 +1,3 @@
---TROISIEME PARTIE
---Requête 1 : Les filières qui attirent les meilleurs candidats
-SELECT
-	f.filiere_formation,
-	SUM(i.eff_admis_neo_bac_TB + i.eff_admis_neo_bac_B) AS total_meilleurs_candidats
-FROM
-	Infos_Formation_Etablissement i
-JOIN
-	Formation f ON i.id_formation = f.id_formation
-GROUP BY
-	f.filiere_formation
-ORDER BY
-	total_meilleurs_candidats DESC;
-
--- Requête 2 : Les filières qui attirent les bacheliers technologiques (néobacheliers)
-SELECT
-	f.filiere_formation,
-	SUM(i.eff_tot_candi_neo_bac_tech_phase_ppl_form) AS total_candidatures_tech
-FROM
-	Infos_Formation_Etablissement i
-JOIN
-	Formation f ON i.id_formation = f.id_formation
-GROUP BY
-	f.filiere_formation
-ORDER BY
-	total_candidatures_tech DESC;
-
-
 -- DEUXIEME PARTIE
 -- Calcul du nombre total de candidatures par filière de formation
 SELECT F.filiere_formation, SUM(IFE.eff_tot_candi_form) AS total_candidatures
@@ -78,3 +50,43 @@ ORDER BY total_candidatures_feminin DESC;
     ORDER BY
         total_candidatures_femmes DESC
     LIMIT 10; -- Pour les filières les plus demandées par les femmes
+
+--TROISIEME PARTIE
+--Requête 1 : Les filières qui attirent les meilleurs candidats
+SELECT
+	f.filiere_formation,
+	SUM(i.eff_admis_neo_bac_TB + i.eff_admis_neo_bac_B) AS total_meilleurs_candidats
+FROM
+	Infos_Formation_Etablissement i
+JOIN
+	Formation f ON i.id_formation = f.id_formation
+GROUP BY
+	f.filiere_formation
+ORDER BY
+	total_meilleurs_candidats DESC;
+
+-- Requête 2 : Les filières qui attirent les bacheliers technologiques (néobacheliers)
+SELECT
+	f.filiere_formation,
+	SUM(i.eff_tot_candi_neo_bac_tech_phase_ppl_form) AS total_candidatures_tech
+FROM
+	Infos_Formation_Etablissement i
+JOIN
+	Formation f ON i.id_formation = f.id_formation
+GROUP BY
+	f.filiere_formation
+ORDER BY
+	total_candidatures_tech DESC;
+
+-- Requête 3 : Les filières qui attirent les bacheliers professionnels (néobacheliers)
+SELECT
+	f.filiere_formation,
+	SUM(i.eff_tot_candi_neo_bac_pro_phase_ppl_form) AS total_candidatures_pro
+FROM
+	Infos_Formation_Etablissement i
+JOIN
+	Formation f ON i.id_formation = f.id_formation
+GROUP BY
+	f.filiere_formation
+ORDER BY
+	total_candidatures_pro DESC;
