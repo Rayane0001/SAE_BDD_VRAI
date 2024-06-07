8e0f5406c4ed9b11c7d074f00cf3b66bf0349c54
Rousseau Rayane <rayane_rousseau@ens.univ-artois.fr>
2024-06-07 10:44:52 +0200

    Test

--- requetesSAE.sql
+++ requetesSAE.sql
@@ -51,6 +51,51 @@ ORDER BY total_candidatures_feminin DESC;
         total_candidatures_femmes DESC
     LIMIT 10; -- Pour les filières les plus demandées par les femmes
 
+    -- A TESTER
+    -- Pour les filières les plus demandées par les hommes
+    SELECT
+        f.filiere_formation,
+        SUM(i.eff_candidate_form) AS total_candidatures_hommes
+    FROM
+        Infos_Formation_Etablissement i
+    JOIN
+        Formation f ON i.id_formation = f.id_formation
+    JOIN
+        Etablissement e ON i.code_uai_etab = e.code_uai_etab
+    JOIN
+        Departement d ON e.code_dept = d.code_dept
+    JOIN
+        Region r ON d.id_region = r.id_region
+    WHERE
+        r.nom_region = 'Hauts-de-France'
+    GROUP BY
+        f.filiere_formation
+    ORDER BY
+        total_candidatures_hommes DESC
+    LIMIT 10;
+
+    -- Pour les filières les moins demandées par les hommes
+    SELECT
+        f.filiere_formation,
+        SUM(i.eff_candidate_form) AS total_candidatures_hommes
+    FROM
+        Infos_Formation_Etablissement i
+    JOIN
+        Formation f ON i.id_formation = f.id_formation
+    JOIN
+        Etablissement e ON i.code_uai_etab = e.code_uai_etab
+    JOIN
+        Departement d ON e.code_dept = d.code_dept
+    JOIN
+        Region r ON d.id_region = r.id_region
+    WHERE
+        r.nom_region = 'Hauts-de-France'
+    GROUP BY
+        f.filiere_formation
+    ORDER BY
+        total_candidatures_hommes ASC
+    LIMIT 10;
+
 --TROISIEME PARTIE
 --Requête 1 : Les filières qui attirent les meilleurs candidats
 SELECT
