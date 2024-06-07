6cbb92a03cf0ae69d3fe2e3ec75bfcaa4daca8d3
MeijiRestored <42336759+MeijiRestored@users.noreply.github.com>
2024-06-07 10:50:07 +0200

    requqete quui marcheos

--- requetesSAE.sql
+++ requetesSAE.sql
@@ -13,45 +13,6 @@ WHERE IFE.code_uai_etab IN (
 GROUP BY F.filiere_formation
 ORDER BY total_candidatures DESC;
 
--- Candidatures par filière de formation pour les candidates féminines
-SELECT F.filiere_formation, SUM(IFE.eff_candidate_form) AS total_candidatures_feminin
-FROM Infos_Formation_Etablissement IFE
-INNER JOIN Formation F ON IFE.id_formation = F.id_formation
-WHERE IFE.code_uai_etab IN (
-	SELECT E.code_uai_etab
-    FROM Etablissement E
-    INNER JOIN Departement D ON E.code_dept = D.code_dept
-    INNER JOIN Region R ON D.id_region = R.id_region
-    WHERE R.nom_region = 'Hauts-de-France'
-)
-GROUP BY F.filiere_formation
-ORDER BY total_candidatures_feminin DESC;
-
-
--- Candidatures par filière de formation pour les candidats masculins
-
-	SELECT
-        f.filiere_formation,
-        SUM(i.eff_tot_candi_femmes) AS total_candidatures_femmes
-    FROM
-        Infos_Formation_Etablissement i
-    JOIN
-        Formation f ON i.id_formation = f.id_formation
-    JOIN
-        Etablissement e ON i.code_uai_etab = e.code_uai_etab
-    JOIN
-        Departement d ON e.code_dept = d.code_dept
-    JOIN
-        Region r ON d.id_region = r.id_region
-    WHERE
-        r.nom_region = 'Hauts-de-France'
-    GROUP BY
-        f.filiere_formation
-    ORDER BY
-        total_candidatures_femmes DESC
-    LIMIT 10; -- Pour les filières les plus demandées par les femmes
-
-    -- A TESTER
     -- Pour les filières les plus demandées par les hommes
     SELECT
         f.filiere_formation,
