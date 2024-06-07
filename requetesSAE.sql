85e92c09a3fcf3dd2807cbd2b4bef81c55073cf3
Rousseau Rayane <rayane_rousseau@ens.univ-artois.fr>
2024-06-07 10:09:49 +0200

    Noubeau fichier

---
+++ requetesSAE.sql
@@ -0,0 +1,26 @@
+--TROISIEME PARTIE
+--Requête 1 : Les filières qui attirent les meilleurs candidats
+SELECT
+	f.filiere_formation,
+	SUM(i.eff_admis_neo_bac_TB + i.eff_admis_neo_bac_B) AS total_meilleurs_candidats
+FROM
+	Infos_Formation_Etablissement i
+JOIN
+	Formation f ON i.id_formation = f.id_formation
+GROUP BY
+	f.filiere_formation
+ORDER BY
+	total_meilleurs_candidats DESC;
+
+-- Requête 2 : Les filières qui attirent les bacheliers technologiques (néobacheliers)
+SELECT
+	f.filiere_formation,
+	SUM(i.eff_tot_candi_neo_bac_tech_phase_ppl_form) AS total_candidatures_tech
+FROM
+	Infos_Formation_Etablissement i
+JOIN
+	Formation f ON i.id_formation = f.id_formation
+GROUP BY
+	f.filiere_formation
+ORDER BY
+	total_candidatures_tech DESC;
